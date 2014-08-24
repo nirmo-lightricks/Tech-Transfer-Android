0cb18a4e3a18a72fc40d1e99be520789486b66d3
Nir Bruner <nbruner@lightricks.com>
2014-08-24 12:23:32 +0300

    Harden ProGuard obfuscation settings.

--- tools/proguard_games/play.py
+++ tools/proguard_games/play.py
@@ -13,11 +13,12 @@ def print_range(start, finish):
 def print_chinese():
     print_range(0x5000, 0x62ff)
 
-def print_ethiopic():
-    print_range(0x1200, 0x137f)
+def print_yi():
+    print_range(0xa000, 0xa48f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Print obfuscation dictionary for ProGuard.")
     args = parser.parse_args()
 
-    print_ethiopic()
+    print_yi()
+    print_chinese()
