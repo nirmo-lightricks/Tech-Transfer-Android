0a647f8ed1680ae411916b4a5e95a311b0004b09
yardenGerecht <yarden@lightricks.com>
2018-10-14 13:30:52 +0300

    Facetune: Change folders structure to support multiple projects.
    Rename app directory to facetune, for better support of multiple
    projects in the same repository.

--- tools/string_import/runme.sh
+++ tools/string_import/runme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python string_import.py --ios=/Users/Nir/Development/lightricks-development/apps/FaceControl/FaceControl/es.lproj --android=/Users/Nir/Development/facetune-android/app/res/values-es/localizable_strings_es.xml --format=/Users/Nir/Development/facetune-android/app/res/values/localizable_strings.xml
+python string_import.py --ios=/Users/Nir/Development/lightricks-development/apps/FaceControl/FaceControl/es.lproj --android=/Users/Nir/Development/facetune-android/facetune/res/values-es/localizable_strings_es.xml --format=/Users/Nir/Development/facetune-android/facetune/res/values/localizable_strings.xml
