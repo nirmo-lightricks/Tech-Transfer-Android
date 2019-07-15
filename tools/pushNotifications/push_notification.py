7158c3fc41984532f74a503930aff8773e6ea309
David Gang <dgang@lightricks.com>
2019-07-15 15:23:35 +0300

    Pixaloop: push notification client side

--- tools/pushNotifications/push_notification.py
+++ tools/pushNotifications/push_notification.py
@@ -19,6 +19,7 @@ logging.basicConfig(level=logging.INFO)
 #limit of tokens for fcm calls
 TOKENS_PER_FCM_CALL = 100
 DEFAULT_TRANSLATION = 'en'
+DATA_KEY = 'data'
 
 def _parse_language_file(language_file: str) -> Dict[str, Dict[str, str]]:
     translations: Dict[str, Dict[str, str]] = defaultdict(dict)
@@ -84,7 +85,7 @@ def send_push_messages(push_message_configuration: PushMessageConfiguration) -> 
     firebase_admin.initialize_app()
     tokens = _get_tokens(push_message_configuration.token_file)
     push_message = _create_push_message(push_message_configuration)
-    data_dict = {'data':push_message.to_json()}
+    data_dict = {DATA_KEY: push_message.to_json()}
     for token_chunk in chunked(tokens, TOKENS_PER_FCM_CALL):
         if push_message_configuration.dry_run:
             logging.info(f'Would have sent {data_dict} to {token_chunk}')
@@ -102,10 +103,10 @@ def _create_argument_parser() -> argparse.ArgumentParser:
     parser.add_argument('content_key')
     parser.add_argument('channel', help='One of the channels: news, promotions')
     parser.add_argument('name')
+    parser.add_argument('image_url')
     parser.add_argument(
-        '--time', help="when to run push notification locally. In format hh:mm")
-    parser.add_argument('--image_url')
-    parser.add_argument('--video_url')
+        '--time', help="when to run push notification locally. In format hh:mm", default='')
+    parser.add_argument('--video_url', default='')
     parser.add_argument('--dry_run', action='store_true', default=False)
     return parser
 
