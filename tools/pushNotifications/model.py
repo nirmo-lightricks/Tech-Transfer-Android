fc73b8b3287b619e7cf14db074574db1b079ccac
Nir Moshe <nmoshe@lightricks.com>
2019-08-08 11:13:00 +0300

    PEP8 + Nice example.

--- tools/pushNotifications/model.py
+++ tools/pushNotifications/model.py
@@ -1,22 +1,23 @@
-
 # Copyright (c) 2019 Lightricks. All rights reserved.
+from dataclasses import dataclass, asdict
+from typing import Dict, List
 import json
 import os
 import re
-from typing import Dict, List
+
 import requests
-from dataclasses import dataclass, asdict
 
 # valid datetime regexp: 10:20 and 8:15 is valid but 10:0 is not valid
 TIME_REGEXP = r'\d{1,2}:\d{2}'
 ALL_CHANNELS = {'news', 'promotions'}
 
+
 @dataclass
-class PushMessageConfiguration():
-    '''
+class PushMessageConfiguration:
+    """
     Describes the input of the program.
     This makes the module agnostic of it's execution environment.
-    '''
+    """
     name: str
     language_file: str
     token_file: str
@@ -30,7 +31,6 @@ class PushMessageConfiguration():
     visible_feature_item_id: str
     dry_run: bool = False
 
-
     def __post_init__(self) -> None:
         if not os.path.isfile(self.language_file):
             raise Exception(
@@ -41,7 +41,7 @@ class PushMessageConfiguration():
             raise Exception('title_key missing')
         if not self.content_key:
             raise Exception('content_key missing')
-        if  not  self.channel:
+        if not self.channel:
             raise Exception('channel is missing')
         if not self.channel in ALL_CHANNELS:
             raise Exception(f' channel should be one of {ALL_CHANNELS} but is {self.channel}')
@@ -53,6 +53,7 @@ class PushMessageConfiguration():
         if self.video_url and not requests.head(self.image_url).ok:
             raise Exception(f'video url {self.image_url} is incorrect')
 
+
 @dataclass
 class PushMessage:
     translations: Dict[str, List[str]]
@@ -63,5 +64,6 @@ class PushMessage:
     video_url: str
     active_feature_id: str
     visible_feature_item_id: str
-    def to_json(self)->str:
-        return json.dumps(asdict(self))
+
+    def to_json(self) -> str:
+        return json.dumps(asdict(self), indent=4)
