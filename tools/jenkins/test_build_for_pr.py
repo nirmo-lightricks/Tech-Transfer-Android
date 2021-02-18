8b647be28f091f934d8fd1e063b775058efc03d8
David Gang <dgang@lightricks.com>
2021-02-18 20:00:28 +0200

    AI-184: Fix python unit tests (#6637)

--- tools/jenkins/test_build_for_pr.py
+++ tools/jenkins/test_build_for_pr.py
@@ -3,23 +3,31 @@ from build_for_pr import _get_gradle_arguments, GRADLE_PR_TASK_NAME
 
 
 class TestBuildForPr(unittest.TestCase):
-    def test_large_test(self):
+    def test_large_test(self) -> None:
         res = _get_gradle_arguments(["swish"], True, set())
-        expected = ["clean", f":swish:{GRADLE_PR_TASK_NAME}"]
-        self.assertListEqual(expected, res)
-
-    def test_small_test(self):
-        res = _get_gradle_arguments(["swish"], False, set())
         expected = [
-            "-Pandroid.testInstrumentationRunnerArguments.notAnnotation=androidx.test.filters.LargeTest",
+            "-Pandroid.testInstrumentationRunnerArguments.notAnnotation=com.lightricks.swish.utils.BoostedLargeTest",
             "clean",
             f":swish:{GRADLE_PR_TASK_NAME}",
         ]
         self.assertListEqual(expected, res)
 
-    def test_assets(self):
+    def test_small_test(self) -> None:
+        res = _get_gradle_arguments(["swish"], False, set())
+        expected = [
+            "-Pandroid.testInstrumentationRunnerArguments.notAnnotation=androidx.test.filters.LargeTest,com.lightricks.swish.utils.BoostedLargeTest",
+            "clean",
+            f":swish:{GRADLE_PR_TASK_NAME}",
+        ]
+        self.assertListEqual(expected, res)
+
+    def test_assets(self) -> None:
         res = _get_gradle_arguments(["swish", "assets"], True, set(["assets"]))
-        expected = ["clean", f":swish:{GRADLE_PR_TASK_NAME}"]
+        expected = [
+            "-Pandroid.testInstrumentationRunnerArguments.notAnnotation=com.lightricks.swish.utils.BoostedLargeTest",
+            "clean",
+            f":swish:{GRADLE_PR_TASK_NAME}",
+        ]
         self.assertListEqual(expected, res)
 
 
